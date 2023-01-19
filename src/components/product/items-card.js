775d3d07d2109d031a152c26e77552f030dbc26a
David Franco Ospina <david.franco@correounivalle.edu.co>
2023-01-19 15:49:19 -0500

    added items card, addapted from products-dard

---
+++ src/components/product/items-card.js
@@ -0,0 +1,96 @@
+import PropTypes from 'prop-types';
+import { Avatar, Box, Card, CardContent, Divider, Grid, Typography } from '@mui/material';
+import { Clock as ClockIcon } from '../../icons/clock';
+import { Download as DownloadIcon } from '../../icons/download';
+
+export const ItemsCard = ({ item, ...rest }) => (
+  <Card
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100%'
+    }}
+    {...rest}
+  >
+    <CardContent>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          pb: 3
+        }}
+      >
+        <Avatar
+          alt="Product"
+          src={item.media}
+          variant="square"
+        />
+      </Box>
+      <Typography
+        align="center"
+        color="textPrimary"
+        gutterBottom
+        variant="h5"
+      >
+        {item.name}
+      </Typography>
+      <Typography
+        align="center"
+        color="textPrimary"
+        variant="body1"
+      >
+        {item.unit_price}
+      </Typography>
+    </CardContent>
+    <Box sx={{ flexGrow: 1 }} />
+    <Divider />
+    <Box sx={{ p: 2 }}>
+      <Grid
+        container
+        spacing={2}
+        sx={{ justifyContent: 'space-between' }}
+      >
+        <Grid
+          item
+          sx={{
+            alignItems: 'center',
+            display: 'flex'
+          }}
+        >
+          <ClockIcon color="action" />
+          <Typography
+            color="textSecondary"
+            display="inline"
+            sx={{ pl: 1 }}
+            variant="body2"
+          >
+            Updated 2hr ago
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          sx={{
+            alignItems: 'center',
+            display: 'flex'
+          }}
+        >
+          <DownloadIcon color="action" />
+          <Typography
+            color="textSecondary"
+            display="inline"
+            sx={{ pl: 1 }}
+            variant="body2"
+          >
+            Stock:
+            {' '}
+            {item.quantity}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  </Card>
+);
+
+ItemsCard.propTypes = {
+  item: PropTypes.object.isRequired
+};
--- src/pages/products.js
+++ src/pages/products.js
@@ -4,58 +4,71 @@ import { products } from '../__mocks__/products';
 import { ProductListToolbar } from '../components/product/product-list-toolbar';
 import { ProductCard } from '../components/product/product-card';
 import { DashboardLayout } from '../components/dashboard-layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { ItemsCard } from '../components/product/items-card';
 
-const Page = () => (
-  <>
-    <Head>
-      <title>
-        Products | Material Kit
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth={false}>
-        <ProductListToolbar />
-        <Box sx={{ pt: 3 }}>
-          <Grid
-            container
-            spacing={3}
+// {
+//   id: uuid(),
+//   createdAt: '27/03/2019',
+//   description: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
+//   media: '/static/images/products/product_1.png',
+//   title: 'Dropbox',
+//   totalDownloads: '594'
+// },
+
+const Page = () => {
+  const [items, setItems] = useState([]);
+  
+  useEffect(() => {
+    axios
+      .get("http://192.168.1.92:5000/Item/getAll")
+      .then((response) => {
+        console.log(response.data.data);
+        setItems(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Products | Material Kit</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth={false}>
+          <ProductListToolbar />
+          <Box sx={{ pt: 3 }}>
+            <Grid container spacing={3}>
+              {items.map((item) => (
+                <Grid item key={item.id} lg={4} md={6} xs={12}>
+                  <ItemsCard item={item} />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              pt: 3,
+            }}
           >
-            {products.map((product) => (
-              <Grid
-                item
-                key={product.id}
-                lg={4}
-                md={6}
-                xs={12}
-              >
-                <ProductCard product={product} />
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            pt: 3
-          }}
-        >
-          <Pagination
-            color="primary"
-            count={3}
-            size="small"
-          />
-        </Box>
-      </Container>
-    </Box>
-  </>
-);
+            <Pagination color="primary" count={3} size="small" />
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 Page.getLayout = (page) => (
   <DashboardLayout>
